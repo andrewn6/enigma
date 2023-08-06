856b1403f045075f020acf2c34026da101a549e8
andrew <andrewnijmeh1@gmail.com>
2023-08-06 10:26:07 -0400

    finish yew impl

--- src/main.rs
+++ src/main.rs
@@ -37,3 +37,47 @@ fn update_position(projectile: &mut Projectile, dt: f64) {
   projectile.position.x += projectile.velocity.x * dt;
   projectile.velocity.y += projectile.velocity.y * dt;
 }
+
+#[function_component]
+fn BallisticCalculator() -> Html {
+  let wind = use_state(|| 0.0);
+  let elevation = use_state(|| 0.0);
+  let caliber = use_state(|| 0.00762);
+  let ballistic_coefficient = use_state(|| 0.4);
+  let projectile = use_state(|| Projectile {
+    position: Vector2 { x: 0.0, y: 0.0 },
+    velocity: Vector2 {
+      x: 850.0 * (elevation.to_owned().into_inner() * std::f64::consts::PI / 180.0.cos()
+      y: 850.0 * (elevation.to_owned().into_inner() * std::f64::consts::PI / 180.0).sin(),
+    },
+  });
+
+  use_interval(
+      {
+        let projectile = projectile.clone();
+        let wind = wind.clone();
+        let caliber = caliber.clone();
+        let ballistic_coefficient = ballistic_coefficient.clone();
+        
+        move || {
+          let mut projectile = projectile.to_owned().into_inner();
+          let dt = 0.01;
+
+          update_velocity(&mut projectile, dt, wind.into_inner(), caliber.into_inner(), ballistic_coefficient.into_inne());
+          update_position(&mut projectile, dt);
+        }
+      },
+  );
+
+  html! {
+    <div>
+      <div>{format!("Position: ({}, {})", projectile.into_inner().position.x, projectile.into_inner().position.y)}</div>
+    </div>
+  }
+
+
+}
+
+fn main() {
+  yew::Renderer::<BallisticCalculator>::new().render();
+}
